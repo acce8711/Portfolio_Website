59668f31f4c01a137a832824b092fa5d70fde7fb
acce8711 <101413675+acce8711@users.noreply.github.com>
2022-12-21 14:50:09 -0500

    finished up homepage

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 
+/*typing header text on page load*/
 var typed = new Typed(".autoType", {
-    strings: ["Interactive Multimedia and Design student."],
+    strings: ["Interactive Multimedia and Design Student."],
     typeSpeed: 75,
     loop: false
 });
+
+
+//hamburger menu
+//tutorial used: https://www.youtube.com/watch?v=ydZc17rlR5E
+const hamburger = document.getElementById("hamburger");
+const otherLinks = document.getElementById("otherLinks");
+
+
+hamburger.addEventListener("click", () => {
+    otherLinks.classList.toggle("show");
+    hamburger.classList.toggle("show");
+    openNav();
+})
 
 
 /*To do:
