b24930b73dcbd0fb7260a08a830094dcdd2f2199
acce8711 <101413675+acce8711@users.noreply.github.com>
2022-12-20 23:32:03 -0500

    worked on homepage css

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var typed = new Typed(".autoType", {
+    strings: ["Interactive Multimedia and Design student."],
+    typeSpeed: 75,
+    loop: false
+});
+
+
+/*To do:
+give credit to type creator
+cerate variables for colours
+*/ 
