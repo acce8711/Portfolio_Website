62be2b813ff1a937b200102de8e62ba407a5a3c0
acce8711 <101413675+acce8711@users.noreply.github.com>
2023-01-02 20:31:47 -0500

    updated js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 
-var url = window.location.pathname;
-var filename = url.substring(url.lastIndexOf('/')+1);
-if (filename == "index.html") {
-    /*typing header text on page load*/
-    var typed = new Typed(".autoType", {
-        strings: ["Interactive Multimedia and Design Student."],
-        typeSpeed: 75,
-        loop: false
-    });
-}
-
-alert(filename);
-
 //hamburger menu
 //tutorial used: https://www.youtube.com/watch?v=ydZc17rlR5E
 const hamburger = document.getElementById("hamburger");
@@ -22,6 +9,16 @@ hamburger.addEventListener("click", () => {
     otherLinks.classList.toggle("show");
     hamburger.classList.toggle("show");
 })
+
+
+/*typing header text on page load*/
+var typed = new Typed(".autoType", {
+    strings: ["Interactive Multimedia and Design Student."],
+    typeSpeed: 75,
+    loop: false
+});
+
+
 
 
 /*To do:
