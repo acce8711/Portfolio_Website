8a5d7e59e5fb387c3e5fb1271a27c395fd109361
acce8711 <101413675+acce8711@users.noreply.github.com>
2024-06-20 17:04:57 -0400

    removed resume link

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ hamburger.addEventListener("click", () => {
 
 /*typing header text on page load*/
 var typed = new Typed(".autoType", {
-    strings: ["Interactive Multimedia and Design Student."],
-    typeSpeed: 75,
+    //strings: ["Interactive Multimedia and Design Student."],
+    strings: ["Information Technology Student."],
+    typeSpeed: 50,
     loop: false
 });
 
